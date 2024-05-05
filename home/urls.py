c160b5f8c2f2c3037d89630ceba609876bb441b9
Luke Twomey <ltwomey@serverxllc.com>
2024-05-05 01:43:16 +0100

    added home, post method and non functioning search

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('',views.index, name='index'),
     path('detail/',views.detail, name='detail'),
     path('detail/<int:book_id>/',views.specific, name='specific'),
+    path('book_entry/',views.book_entry, name='book_entry'),
+    path('book_entry_process/',views.book_entry_process, name='book_entry_process'),
 ]
--- home/views.py
+++ home/views.py
@@ -3,7 +3,8 @@ from django.template import loader
 from .models import Book
 # this shows hello world
 def index(request):
-    return HttpResponse('Hello World')
+    template = loader.get_template('home/home.html')
+    return HttpResponse(template.render(None, request))
 
 # this loads a template, finds all books in table Book and returns a list of books sorted in html
 def detail(request):
@@ -22,3 +23,25 @@ def detail(request):
 def specific(request, book_id):
     response = "You're looking at the book %s."
     return HttpResponse(response % book_id)
+
+def search(request):
+    response = "You're looking at the book %s."
+    return HttpResponse(response % book_id)
+
+def book_entry(request):
+    template = loader.get_template('home/bookentry.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def book_entry_process(request):
+    if request.method == 'POST':
+        book_name = request.POST.get('book_name')
+        book_id = request.POST.get('book_id')
+        
+        # Create a new patient entry in the database using the Patient model
+        book = Book(book_id=book_id, book_name=book_name)
+        book.save()
+
+        return HttpResponse("Data successfully inserted!")
+    else:
+        return HttpResponse("Invalid request method.")
